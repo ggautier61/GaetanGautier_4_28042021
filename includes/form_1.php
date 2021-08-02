5cd90209171e9db8fea9d551e38496a8fa805f47
Gaetan GAUTIER <gaetang085@gmail.com>
2021-08-02 12:21:57 +0200

    Fix Nav Bar Button collapse

--- includes/form_1.php
+++ includes/form_1.php
@@ -1,7 +1,7 @@
 <?php	
 	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['input_504']) && strlen($_POST['input_504']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
 	{
-		return false;
+		return true;
 	}
 	
 	$name = $_POST['name'];
